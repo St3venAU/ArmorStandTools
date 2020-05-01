7caec9b96a37722031e27ccc17fe8837565a3c18
Pollitoyeye <pollitoyeye@gmail.com>
2020-05-01 16:36:27 +0200

    Updated PlotSquaredHook to V5 API.

--- src/com/gmail/St3venAU/plugins/ArmorStandTools/PlotSquaredHook.java
+++ src/com/gmail/St3venAU/plugins/ArmorStandTools/PlotSquaredHook.java
@@ -1,12 +1,13 @@
 package com.gmail.St3venAU.plugins.ArmorStandTools;
 
-import com.github.intellectualsites.plotsquared.api.PlotAPI;
-import com.github.intellectualsites.plotsquared.bukkit.util.BukkitUtil;
-import com.github.intellectualsites.plotsquared.plot.object.Plot;
-import com.github.intellectualsites.plotsquared.plot.object.PlotArea;
-import com.github.intellectualsites.plotsquared.plot.object.PlotPlayer;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+
+import com.plotsquared.bukkit.util.BukkitUtil;
+import com.plotsquared.core.api.PlotAPI;
+import com.plotsquared.core.player.PlotPlayer;
+import com.plotsquared.core.plot.Plot;
+import com.plotsquared.core.plot.PlotArea;
 
 import java.util.UUID;
 
@@ -25,7 +26,7 @@ class PlotSquaredHook {
     }
 
     public static boolean checkPermission(Player player, Location location) {
-        com.github.intellectualsites.plotsquared.plot.object.Location plotLocation = BukkitUtil.getLocation(location);
+    	com.plotsquared.core.location.Location plotLocation = BukkitUtil.getLocation(location);
         PlotArea plotArea = plotLocation.getPlotArea();
         if(plotArea == null) {
             plugin.debug("plots.admin.build.road: " + player.hasPermission("plots.admin.build.road"));
