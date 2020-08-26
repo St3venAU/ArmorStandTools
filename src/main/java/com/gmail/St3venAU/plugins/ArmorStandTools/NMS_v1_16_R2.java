9ae604a1bad74ea41c26401b4444aafd06a33f4d
Matthew Hildebrand <korvic@live.ca>
2020-08-26 09:58:49 -0500

    Remove a bit of deprecation. Add NMS for 1.16.2

---
+++ src/main/java/com/gmail/St3venAU/plugins/ArmorStandTools/NMS_v1_16_R2.java
@@ -0,0 +1,13 @@
+package com.gmail.St3venAU.plugins.ArmorStandTools;
+
+@SuppressWarnings("unused")
+class NMS_v1_16_R2 extends NMS {
+
+    public NMS_v1_16_R2(String nmsVersion) {
+        super(
+                nmsVersion,
+                "bA"
+        );
+    }
+
+}
